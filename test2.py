cb7452542a066d00db0eee684ad2742f606b7763
Leo <leooooo@bu.edu>
2015-10-13 20:03:56 -0400

    testing some ideas

---
+++ test2.py
@@ -0,0 +1,11 @@
+import tkinter
+root = tkinter.Tk()
+label = tkinter.Label(text="testing", bg='white')
+root.overrideredirect(True)
+root.geometry("+250+250")
+root.lift()
+root.wm_attributes("-topmost", True)
+#root.wm_attributes("-disabled", True)
+root.wm_attributes("-transparentcolor", "white")
+label.pack()
+label.mainloop()
